d3510ca09e842d95456d9112e4a03839939436c4
Tyler Cutrer <wtcutrer@gmail.com>
2021-07-18 14:15:08 -0500

    PyBank Complete - Need to Streamline

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,78 @@
-print("PyBank HW")
+#Importing correct modules
+import os
+import csv
+
+#Creating an object out of the CSV file
+budget_data = os.path.join("PyBank","Resources","budget_data.csv")
+
+total_months = 0
+net_change = 0
+value = 0
+change = 0
+dates = []
+profits = []
+
+#Ready CSV file for use
+with open(budget_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    #Reading the header row
+    csv_header = next(csvreader)
+
+    #Read first row
+    first_row = next(csvreader)
+    total_months += 1
+    net_change += int(first_row[1])
+    value = int(first_row[1])
+    
+    #Loop through each row
+    for row in csvreader:
+        
+        # Keeping track of the dates
+        dates.append(row[0])
+        
+        # Calculate the change
+        change = int(row[1])-value
+        profits.append(change)
+        value = int(row[1])
+        
+        #Total number of months
+        total_months += 1
+
+        #Total net change
+        net_change = net_change + int(row[1])
+
+    #Greatest increase in profits
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = dates[greatest_index]
+
+    #Greatest decrease (lowest increase) in profits 
+    greatest_decrease = min(profits)
+    worst_index = profits.index(greatest_decrease)
+    worst_date = dates[worst_index]
+
+    #Average change in "Profit/Losses between months over entire period"
+    avg_change = sum(profits)/len(profits)
+    
+#Displaying information
+print("Financial Analysis")
+print("---------------------")
+print(f"Total Months: {str(total_months)}")
+print(f"Total: ${str(net_change)}")
+print(f"Average Change: ${str(round(avg_change,2))}")
+print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+
+#Send to .txt file
+output = open("Financial Analysis.txt", "w")
+
+line1 = "Financial Analysis"
+line2 = "---------------------"
+line3 = str(f"Total Months: {str(total_months)}")
+line4 = str(f"Total: ${str(net_change)}")
+line5 = str(f"Average Change: ${str(round(avg_change,2))}")
+line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+output.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
+
