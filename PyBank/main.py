a790db6a5036285bcb8630d0cbec1d85363b680c
Tyler Cutrer <wtcutrer@gmail.com>
2021-07-20 16:29:10 -0500

    Solved issue with PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,10 +4,10 @@ import csv
 
 #Create lists
 months = []
-profit_loss = []
+margin_shift = []
 
 #Set path 
-budget_csv = os.path.join('budget_data.csv')
+budget_csv = os.path.join('Resources','budget_data.csv')
 
 #Open CSV
 with open(budget_csv, newline="") as csvfile:
@@ -23,46 +23,46 @@ with open(budget_csv, newline="") as csvfile:
         months.append(row[0])
 
         #Add Profit/Loss
-        profit_loss.append(float(row[1]))
+        margin_shift.append(float(row[1]))
 
 #Calculate total months 
 total_months = (len(months))
 
 #Calculate the net amount
-net_amount = sum(profit_loss)
+net_amount = sum(margin_shift)
 
 #Calculate the average change
 avg_change = net_amount / total_months
 
 #Calculate the greatest increase
-max_profit = max(profit_loss)
+greatest_profit = max(margin_shift)
 
 #Using the index of the greatest increase
-index_max = profit_loss.index(max_profit)
-max_month = months[index_max]
+index_max = margin_shift.index(greatest_profit)
+best_month = months[index_max]
 
 #Calculate the greatest decrease
-min_profit = min(profit_loss)
+least_profit = min(margin_shift)
 
 #Find Date
-index_min = profit_loss.index(min_profit)
-min_month = months[index_min]
+index_min = margin_shift.index(least_profit)
+worst_month = months[index_min]
 
-financial_analysis = (f'''Financial Analysis
+budget_analysis = (f'''Financial Analysis
 ----------------------------------
 Total Months: {total_months}
 Total: ${net_amount:.2f}
 Average Change: {avg_change:.2f}
-Greatest Increase in Profits: {max_month} {max_profit:.2f}
-Greatest Decrease in Profits: {min_month} {min_profit:.2f}''')
+Greatest Increase in Profits: {best_month} {greatest_profit:.2f}
+Greatest Decrease in Profits: {worst_month} {least_profit:.2f}''')
 
 #Print out analysis
-print(financial_analysis)
+print(budget_analysis)
 
 #Create a .txt file containing the same analysis in the print out
-analysis = open('financial_analysis.txt', 'w')
+analysis = open('Budget Analysis.txt', 'w')
 
-analysis.write(financial_analysis)
+analysis.write(budget_analysis)
 
 analysis.close()
 
